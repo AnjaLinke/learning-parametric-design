c20e48d6a9602a1a3ee1551b30edd4db0a95f889
Anja Linke <anjalinke@Anjas-MacBook-Air.local>
2022-11-09 08:39:38 +0100

    Hausaufgabe #1 Update

--- code/example/sketch.js
+++ code/example/sketch.js
@@ -9,17 +9,8 @@ function setup() {
 function draw() {
   background ("#ffd1dc");
 
-
-  fill (150, 0, 50, 100);
-  square (50, 50, 300);
-  circle (200, 200, 300);
-  
-  fill (5, 6, 13);
-  circle (200, 200, 200);
-
-  fill (80, 0, 40, 100)
-  rect (75, 150, 250, 100)
-
+  mybackground ()
+  mysquare ()
 
   stroke ('red');
 
@@ -36,8 +27,8 @@ function draw() {
   circle (200, 200, 10);
   fill (255);
   
-  
-  stroke (0);
+  strokeWeight (2)
+  stroke (255);
 
   line (400, 0, 350, 200);
   line (350, 200, 400, 400);
@@ -61,4 +52,31 @@ function draw() {
   line (400, 230, 230, 400);
   line (170, 400, 0, 230);
   line (0, 170, 170, 0);
+  
+}
+
+function mysquare (){
+  stroke ('blue')
+
+  fill (150, 0, 50, 100);
+  square (50, 50, 300);
+  circle (200, 200, 300);
+  
+  fill (5, 6, 13);
+  circle (200, 200, 200);
+
+  fill (80, 0, 40, 100)
+  rect (75, 150, 250, 100)
+  
+}
+
+function mybackground (){
+  strokeWeight (1)
+  stroke ('black');
+  for (x=0;x<400;x+=4) {
+    line (0, 0, x, 400);
+    line (400, 0, x, 400);
+    line (0, 400, x, 0);
+    line (400, 400, x, 0);
+  }
 }
